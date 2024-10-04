e8eea877fa97b826186d54b4252f05f59902f0cf
mindev188 <mindev188@gmail.com>
2024-10-04 12:41:48 +0900

    프로그래머스 Day 4 연산, 조건문 2차 커밋
    4. 조건 문자열
    5. flag에 따라 다른 값 변환하기

---
+++ src/stepD/ConditionString.java
@@ -0,0 +1,69 @@
+package stepD;
+
+/**
+ * 문제 설명
+ * 문자열에 따라 다음과 같이 두 수의 크기를 비교하려고 합니다.
+
+ * 두 수가 n과 m이라면
+ * ">", "=" : n >= m
+ * "<", "=" : n <= m
+ * ">", "!" : n > m
+ * "<", "!" : n < m
+ * 두 문자열 ineq와 eq가 주어집니다. ineq는 "<"와 ">"중 하나고, eq는 "="와 "!"중 하나입니다. 그리고 두 정수 n과 m이 주어질 때, n과 m이 ineq와 eq의 조건에 맞으면 1을 아니면 0을 return하도록 solution 함수를 완성해주세요.
+
+ * 제한 사항
+ * 1 ≤ n, m ≤ 100
+ * 
+ * 입출력 예
+ * ineq	eq	n	m	result
+ * "<"	"="	20	50	1
+ * ">"	"!"	41	78	0
+ */
+public class ConditionString {
+
+    public static void main(String[] args) {
+        System.out.println(solution("<", "=", 20, 50));
+        System.out.println(solution(">", "!", 41, 78));
+    }
+
+    private static int solution(String ineq, String eq, int n, int m) {
+        int answer = 0;
+
+        /*
+         * 조건 
+         * ineq 대소 비교 및 eq 여부에 따라 조건이 일치하는 경우 1 반환
+         * <=, >=
+         * <, >
+         */
+        if ("<".equals(ineq)) {
+            if ("=".equals(eq)) {
+                answer = n <= m ? 1 : 0;
+            } else {
+                answer = n < m ? 1 : 0;
+            }
+        } else {
+            if ("=".equals(eq)) {
+                answer = n >= m ? 1 : 0;
+            } else {
+                answer = n > m ? 1 : 0;
+            }
+        }
+
+        /*
+         * Other Code
+         * 
+         * boolean answer = false;
+         * if (ineq.equals(">") && eq.equals("="))
+         *    answer = n >= m;
+         * else if (ineq.equals("<") && eq.equals("="))
+         *    answer = n <= m;
+         * else if (ineq.equals(">") && eq.equals("!"))
+         *    answer = n > m;
+         * else 
+         *    answer = n < m;
+         * return answer ? 1 : 0;
+         */
+
+        return answer;
+    }
+}
