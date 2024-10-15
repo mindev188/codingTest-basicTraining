511921e85f4e1423a22b556da21d1d485227f4e1
mindev188 <mindev188@gmail.com>
2024-10-15 23:50:34 +0900

    프로그래머스 Day 10 문자열 1차 커밋
    1. 문자열의 앞의 n글자
    2. 접두사인지 확인하기
    3. 문자열 뒤집기

---
+++ src/stepJ/ReverseString.java
@@ -0,0 +1,51 @@
+package stepJ;
+
+/**
+ * 문제 설명
+ * 문자열 my_string과 정수 s, e가 매개변수로 주어질 때, my_string에서 인덱스 s부터 인덱스 e까지를 뒤집은 문자열을 return 하는 solution 함수를 작성해 주세요.
+
+ * 제한사항
+ * my_string은 숫자와 알파벳으로만 이루어져 있습니다.
+ * 1 ≤ my_string의 길이 ≤ 1,000
+ * 0 ≤ s ≤ e < my_string의 길이
+ * 입출력 예
+ * my_string	s	e	result
+ * "Progra21Sremm3"	6	12	"ProgrammerS123"
+ * "Stanley1yelnatS"	4	10	"Stanley1yelnatS"
+ * 입출력 예 설명
+ * 입출력 예 #1
+
+ * 예제 1번의 my_string에서 인덱스 6부터 인덱스 12까지를 뒤집은 문자열은 "ProgrammerS123"이므로 "ProgrammerS123"를 return 합니다.
+ * 입출력 예 #2
+
+ * 예제 2번의 my_string에서 인덱스 4부터 인덱스 10까지를 뒤집으면 원래 문자열과 같은 "Stanley1yelnatS"이므로 "Stanley1yelnatS"를 return 합니다.
+ */
+public class ReverseString {
+
+    public static void main(String[] args) {
+        System.out.println(solution("Progra21Sremm3", 6, 12));
+        System.out.println(solution("Stanley1yelnatS", 4, 10));
+    }
+
+    private static String solution(String my_string, int s, int e) {
+        String answer = "";
+        // 전체 문자열에서 s 부터 e까지의 문자 위치를 변환
+        char[]  arr = my_string.toCharArray();
+        while (s < e) {
+            char tmp;
+            tmp = arr[s];
+            arr[s] = arr[e];
+            arr[e] = tmp;
+            s++;e--;
+        }
+        answer = new String(arr);
+
+        /**
+         * Other Code
+         * StringBuilder answer = new StringBuilder(my_string.substring(s, e + 1));
+         * answer.reverse();
+         * return my_string.substring(0, s) + answer + my_string.substring(e + 1);
+         */
+        return answer;
+    }
+}
